16737d93cd71621638e1bac3c047ff685866436c
bruno ciccio <dev.bruno.ciccio@gmail.com>
2024-11-07 02:43:30 -0300

    config home

--- src/main/java/br/com/dicionariofutebolistico/DicionariofutebolisticoApplication.java
+++ src/main/java/br/com/dicionariofutebolistico/DicionariofutebolisticoApplication.java
@@ -2,12 +2,22 @@ package br.com.dicionariofutebolistico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class DicionariofutebolisticoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DicionariofutebolisticoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        // Inicializa a aplicação Spring Boot sem carregar o arquivo .env
+        SpringApplication.run(DicionariofutebolisticoApplication.class, args);
+    }
 
+    @RequestMapping("/home")
+    @ResponseBody
+    public String home() {
+        return "API Full-Stack do Projeto DicioFutebolistico";
+    }
 }
